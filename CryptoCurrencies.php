17f35e13338f030506c54003352a29cc79c4a18b
Rudger Gravestein <r.gravestein@triple-networks.com>
2021-09-19 13:42:19 +0200

    Initial commit

---
+++ CryptoCurrencies.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Symfony\Polyfill\Intl\Crypto;
+
+
+class CryptoCurrencies
+{
+    private static $data;
+
+    public static function getSymbol(string $currency): ?string
+    {
+        $data = self::$data ?? self::$data = require __DIR__.'/Resources/cryptocurrencies.php';
+
+        return $data[$currency][0] ?? $data[strtoupper($currency)][0] ?? null;
+    }
+
+    public static function getFractionDigits(string $currency): int
+    {
+        $data = self::$data ?? self::$data = require __DIR__.'/Resources/cryptocurrencies.php';
+
+        return $data[$currency][1] ?? $data[strtoupper($currency)][1] ?? $data['DEFAULT'][1];
+    }
+
+    public static function getRoundingIncrement(string $currency): int
+    {
+        $data = self::$data ?? self::$data = require __DIR__.'/Resources/cryptocurrencies.php';
+
+        return $data[$currency][2] ?? $data[strtoupper($currency)][2] ?? $data['DEFAULT'][2];
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Symfony Polyfill / Intl: ICU Addition for CryptoCurrencies
+==========================================================
+
+This package adds cryptos to the currency formatter.
+It is limited to the "en" locale and to:
+
+- [`NumberFormatter`](https://php.net/NumberFormatter)
+
+License
+=======
+
+This library is released under the [MIT license](LICENSE).
